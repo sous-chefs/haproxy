a41fd07894adce0972ae84eb6c4670a27235eab5
Mike Fiedler <miketheman@gmail.com>
2012-10-23 17:32:42 -0700

    [COOK-1594] Reorder template and service definitions

--- recipes/app_lb.rb
+++ recipes/app_lb.rb
@@ -44,16 +44,13 @@ package "haproxy" do
   action :install
 end
 
-template "/etc/default/haproxy" do
-  source "haproxy-default.erb"
+cookbook_file "/etc/default/haproxy" do
+  source "haproxy-default"
   owner "root"
   group "root"
   mode 00644
-end
-
-service "haproxy" do
-  supports :restart => true, :status => true, :reload => true
-  action [:enable, :start]
+  mode 0644
+  notifies :restart, "service[haproxy]"
 end
 
 template "/etc/haproxy/haproxy.cfg" do
@@ -63,4 +60,9 @@ template "/etc/haproxy/haproxy.cfg" do
   mode 00644
   variables :pool_members => pool_members.uniq
   notifies :restart, "service[haproxy]"
+end
+
+service "haproxy" do
+  supports :restart => true, :status => true, :reload => true
+  action [:enable, :start]
 end
