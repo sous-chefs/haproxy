d6429aa2c5ad55815a67a891cb0802638dfe8707
Guilhem Lettron <guilhem@lettron.fr>
2012-11-21 12:42:16 +0100

    change default address to 127.0.0.1
    stat page musn't be access by anyone by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,7 +19,6 @@
 
 default['haproxy']['incoming_port'] = 80
 default['haproxy']['member_port'] = 8080
-default['haproxy']['enable_admin'] = true
 default['haproxy']['app_server_role'] = "webserver"
 default['haproxy']['balance_algorithm'] = "roundrobin"
 default['haproxy']['member_max_connections'] = 100
@@ -29,3 +28,6 @@ default['haproxy']['ssl_incoming_port'] = 443
 default['haproxy']['ssl_member_port'] = 8443
 default['haproxy']['httpchk'] = nil
 default['haproxy']['ssl_httpchk'] = nil
+default['haproxy']['enable_admin'] = true
+default['haproxy']['admin']['address_bind'] = "127.0.0.1"
+default['haproxy']['admin']['port'] = 22002
