73f0ea89ac57d5284d2140c6df3ae4e0b39adf4c
Guilhem Lettron <guilhem@lettron.fr>
2012-11-22 11:24:42 +0100

    add attribute "pid_file"

--- attributes/default.rb
+++ attributes/default.rb
@@ -27,3 +27,5 @@ default['haproxy']['x_forwarded_for'] = false
 default['haproxy']['enable_ssl'] = false
 default['haproxy']['ssl_incoming_port'] = 443
 default['haproxy']['ssl_member_port'] = 8443
+
+default['haproxy']['pid_file'] = "/var/run/haproxy.pid"
