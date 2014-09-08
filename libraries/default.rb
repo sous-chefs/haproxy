57c724ef5336c42700c45571a79a3f658e2263be
Aaron Baer <aaron@hw-ops.com>
2014-09-08 15:08:31 -0700

    moved methods to provider

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,16 +1,4 @@
 # No namespace!?
-def haproxy_defaults_options
-  options = node['haproxy']['defaults_options'].dup
-  if node['haproxy']['x_forwarded_for']
-    options.push("forwardfor")
-  end
-  return options.uniq
-end
-
-def haproxy_defaults_timeouts
-  node['haproxy']['defaults_timeouts']
-end
-
 module ChefHaproxy
   class << self
     def config_generator(thing, prefix)
