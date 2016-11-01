2ff3974c30231bd1adfe1be8f7b57de7a835e8af
Tim Smith <tsmith@chef.io>
2016-11-01 14:02:48 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: haproxy
 # Default:: default
 #
-# Copyright 2010, Opscode, Inc.
+# Copyright 2010-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
