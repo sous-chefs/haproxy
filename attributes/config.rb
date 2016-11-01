91405a99bf111b73538fbb4062cc310ee3f833a8
Tim Smith <tsmith@chef.io>
2016-11-01 14:08:31 -0700

    Remove extra \
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/config.rb
+++ attributes/config.rb
@@ -15,7 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#\
+#
 
 # Base attributes used when generating configuration using default LWRP
 default[:haproxy][:config][:global][:log]['127.0.0.1'][:local0] = :warn
