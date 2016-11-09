a987c1342a64dcd17f313bf6499e44efc5e94aa9
Tim Smith <tsmith@chef.io>
2016-11-09 11:20:08 -0800

    Update matchers
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,10 +1,15 @@
 if defined?(ChefSpec)
-  def create_haproxy
-    ChefSpec::Matchers::ResourceMatcher.new(:haproxy, :create)
+
+  ChefSpec.define_matcher :haproxy
+  ChefSpec.define_matcher :haproxy_lb
+  ChefSpec.define_matcher :haproxy_config
+
+  def create_haproxy(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:haproxy, :create, name)
   end
 
-  def delete_haproxy
-    ChefSpec::Matchers::ResourceMatcher.new(:haproxy, :delete)
+  def delete_haproxy(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:haproxy, :delete, name)
   end
 
   def create_haproxy_lb(name)
