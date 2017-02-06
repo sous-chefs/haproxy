0faf47004f130b74f755e0819ba085c69a7eaf12
Richard Shade <rshade@rightscale.com>
2017-02-06 08:59:43 -0600

    removing install_package recipe, adding equal_to, and moving package install to recipe

--- resources/default.rb
+++ resources/default.rb
@@ -1,19 +1,41 @@
 provides :haproxy
 
 property :name, String, default: 'default', name_property: true
-property :install_type, String, default: 'package'
+property :install_type, String, default: 'package', equal_to: %w(package source)
 
 action :create do
-  include_recipe "haproxy::install_#{install_type}"
 
   case install_type
   when 'package'
+    package node['haproxy']['package']['name'] do
+      version node['haproxy']['package']['version'] if node['haproxy']['package']['version']
+      action :install
+    end
+
+    directory node['haproxy']['conf_dir'] do
+      owner 'root'
+      group 'root'
+      mode '0755'
+      recursive true
+      action :create
+    end
+    config_file='/etc/haproxy/haproxy.cfg'
+    bin_prefix='/usr/sbin'
   when 'source'
+    include_recipe 'haproxy::install_source'
+    config_file='/usr/local/etc/haproxy/haproxy.cfg'
+    bin_prefix='/usr/local/bin'
+    node.override['haproxy']['poise_service']['options']['sysvinit']['conf_dir'] = '/usr/local/etc/haproxy'
+  end
+  if node['init_package'] == 'systemd'
+    haproxy_command = "#{::File.join(bin_prefix, 'haproxy-systemd-wrapper')} -f #{config_file} -p /run/haproxy.pid $OPTIONS"
+  else
+    haproxy_command = ::File.join(bin_prefix, 'haproxy')
   end
 
   poise_service 'haproxy' do
     provider node['init_package']
-    command "#{haproxy_systemd_wrapper} -f #{haproxy_config_file} -p /run/haproxy.pid $OPTIONS"
+    command haproxy_command
     options node['haproxy']['poise_service']['options'][node['init_package']]
     action :enable
   end
