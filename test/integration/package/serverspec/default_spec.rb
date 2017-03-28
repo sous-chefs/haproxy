ad90c10f565bc1f2b83fcb17c9ec11cd030397db
Erinn Looney-Triggs <erinn.looneytriggs@gmail.com>
2017-03-28 15:19:10 -0600

    Add in serverspec testing

---
+++ test/integration/package/serverspec/default_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe file('/etc/haproxy/haproxy.cfg') do
+  it { is_expected.to exist }
+  it { is_expected.to be_file }
+  its(:content) { is_expected.to match('log 127.0.0.1 local0') }
+  its(:content) { is_expected.to match('log 127.0.0.1 local1 notice') }
+end
