550d1b1983d6cca6987911d4766f9de2eef3991e
Dan Webb <dan.webb@damacus.io>
2017-04-13 21:59:21 +0100

    Chef 12.5 compatability.

--- metadata.rb
+++ metadata.rb
@@ -5,15 +5,14 @@ license           'Apache 2.0'
 description       'Installs and configures haproxy'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '4.0.0'
-source_url        'https://github.com/sous-chefs/haproxy' if respond_to?(:source_url)
-issues_url        'https://github.com/sous-chefs/haproxy/issues' if respond_to?(:issues_url)
+source_url        'https://github.com/sous-chefs/haproxy'
+issues_url        'https://github.com/sous-chefs/haproxy/issues'
 chef_version      '>= 12.5' if respond_to?(:chef_version)
 
 %w( debian ubuntu centos redhat scientific oracle ).each do |os|
   supports os
 end
 
-depends           'compat_resource', '>= 12.16.3'
 depends           'cpu', '>= 0.2.0'
 depends           'build-essential'
 depends           'poise-service'
