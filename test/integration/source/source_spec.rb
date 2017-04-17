5b2f450eac308d36fd81d5e1698dbdca65bf6fc0
Dan Webb <dan.webb@damacus.io>
2017-04-17 19:25:59 +0100

    Tidy up dokken config, remove foodcritic file, use simple spec test

--- test/integration/source/source_spec.rb
+++ test/integration/source/source_spec.rb
@@ -1,3 +1,9 @@
-describe package('haproxy') do
+describe directory '/etc/haproxy' do
+  it { should exist }
+end
+
+describe service 'haproxy' do
   it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
 end
