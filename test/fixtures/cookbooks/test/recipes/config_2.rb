63bd5fd08cc9a06271ee5f1236ac21b2a77e0bf1
Will Fisher <will@gina.alaska.edu>
2017-04-18 12:31:45 -0800

    fix up the config_2 so it will create valid haproxy config

--- test/fixtures/cookbooks/test/recipes/config_2.rb
+++ test/fixtures/cookbooks/test/recipes/config_2.rb
@@ -1,11 +1,20 @@
 haproxy_install 'package'
 
+directory '/etc/haproxy/errors' do
+  user 'haproxy'
+  group 'haproxy'
+end
+
+file '/etc/haproxy/errors/403.http' do
+  content '<h1>Error: 403</h1>'
+end
+
 haproxy_config_global 'global' do
   daemon false
   maxconn 4096
   chroot '/var/lib/haproxy'
-  stats_socket '/var/run/haproxy/haproxy.stat mode 600 level admin'
-  stats_timeout 'timeout 2m'
+  stats_socket '/var/lib/haproxy/haproxy.stat mode 600 level admin'
+  stats_timeout '2m'
 end
 
 haproxy_config_defaults 'defaults' do
@@ -15,7 +24,6 @@ haproxy_config_defaults 'defaults' do
           server: '50s'
   log 'global'
   retries 3
-  extra_options 'stick-table' => 'type ip size 200k expire 10m store gpc0'
 end
 
 haproxy_frontend 'http' do
@@ -25,11 +33,6 @@ haproxy_frontend 'http' do
   use_backend ['gina if gina_host',
                'rrhost if rrhost_host',
                'abuser if source_is_abuser',
-               'abuser if bad_client',
-               'tiles if tile_host authorized_token',
-               'tiles if tile_host authorized_network',
-               'tiles_kml if tile_host kml_request',
-               'tiles_kml if tile_host bbox_request',
                'tiles_public if tile_host']
   option %w(httplog dontlognull forwardfor)
   acl ['kml_request path_reg -i /kml/',
@@ -37,10 +40,7 @@ haproxy_frontend 'http' do
        'gina_host hdr(host) -i foo.bar.com',
        'rrhost_host hdr(host) -i dave.foo.bar.com foo.foo.com',
        'source_is_abuser src_get_gpc0(http) gt 0',
-       'tile_host hdr(host) -i -f /etc/haproxy/tile_domains.lst',
-       'authorized_network src -f /etc/haproxy/authorized_networks.lst',
-       'bad_client src -f /etc/haproxy/banned_ip.lst',
-       'authorized_token urlp(GOGC) -i -f /etc/haproxy/authorized_tokens.lst',
+       'tile_host hdr(host) -i dough.foo.bar.com',
   ]
   extra_options 'stick-table' => 'type ip size 200k expire 10m store gpc0',
                 'tcp-request' => 'connection track-sc1 src if !source_is_abuser'
@@ -49,16 +49,29 @@ end
 haproxy_backend 'tiles_public' do
   server ['tile0 10.0.0.10:80 check weight 1 maxconn 100',
           'tile1 10.0.0.10:80 check weight 1 maxconn 100']
-  tcp_request ['track-sc2 src',
-               'reject if conn_rate_abuse !authorized_network mark_as_abuser']
-  acl ['authorized_network src -f /etc/haproxy/authorized_networks.lst',
-       'conn_rate_abuse sc2_conn_rate gt 3000',
+  tcp_request ['content track-sc2 src',
+               'content reject if conn_rate_abuse mark_as_abuser']
+  acl ['conn_rate_abuse sc2_conn_rate gt 3000',
        'data_rate_abuse sc2_bytes_out_rate gt 20000000',
        'mark_as_abuser sc1_inc_gpc0 gt 0',
      ]
-  extra_options 'stick-table' => 'type ip size 200k expire 2m store conn_rate(60s),bytes_out_rate(60s)'
+  extra_options(
+    'stick-table' => 'type ip size 200k expire 2m store conn_rate(60s),bytes_out_rate(60s)',
+    'http-request' => 'set-header X-Public-User yes',
+  )
+
 end
 
 haproxy_backend 'abuser' do
   extra_options 'errorfile' => '403 /etc/haproxy/errors/403.http'
+end
+
+haproxy_backend 'rrhost' do
+  server ['tile0 10.0.0.10:80 check weight 1 maxconn 100',
+          'tile1 10.0.0.10:80 check weight 1 maxconn 100']
+end
+
+haproxy_backend 'gina' do
+  server ['tile0 10.0.0.10:80 check weight 1 maxconn 100',
+          'tile1 10.0.0.10:80 check weight 1 maxconn 100']
 end
