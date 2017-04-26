4801af4ae32001f50c373075622be33c5f06b65e
Richard Shade <rshade@rightscale.com>
2017-04-26 11:29:22 -0500

    adding search (#204)
    * adding search
    
    * adding in tests, and fixing backends
    
    * remvoing log statement
    
    * fixing styling

---
+++ test/integration/config_backend_search/example_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+title 'Frontend & Backend should be configurable'
+
+describe package('haproxy') do
+  it { should be_installed }
+end
+
+describe directory '/etc/haproxy' do
+  it { should exist }
+end
+
+describe file('/etc/haproxy/haproxy.cfg') do
+  it { should exist }
+  it { should be_owned_by 'haproxy' }
+  it { should be_grouped_into 'haproxy' }
+  its('content') { should match(/daemon/) }
+  its('content') { should match(/timeout connect 5000ms/) }
+  its('content') { should match(/frontend http-in/) }
+  its('content') { should match(/bind \*:80/) }
+  its('content') { should match(/backend servers/) }
+  its('content') { should match(/server be-1 10.0.0.75:8000 maxconn 32/) }
+  its('content') { should match(/server be-2 10.0.0.76:8000 maxconn 32/) }
+end
+
+describe service('haproxy') do
+  it { should be_running }
+end
