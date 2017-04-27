298aa9f40f4f894c822c627e18a49c6ddefa54d8
Richard Shade <rshade@rightscale.com>
2017-04-27 10:25:37 -0500

    making attribute

--- test/fixtures/cookbooks/test/recipes/source.rb
+++ test/fixtures/cookbooks/test/recipes/source.rb
@@ -6,7 +6,8 @@ haproxy_config_defaults '' do
 end
 
 haproxy_install 'source' do
-  source_url 'http://ps-cf.rightscale.com/haproxy/haproxy-1.7.5.tar.gz'
+  source_url node['haproxy']['source_url']
+  source_checksum node['haproxy']['source_checksum']
   use_pcre '1'
   use_openssl '1'
   use_zlib '1'
