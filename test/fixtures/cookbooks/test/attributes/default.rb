7049b8b3f1aad9e52ebc3abbe2f98fa69894eee5
Richard Shade <rshade@rightscale.com>
2017-04-27 10:45:44 -0500

    making env variables

--- test/fixtures/cookbooks/test/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['haproxy']['source_url'] = 'http://www.haproxy.org/download/1.7/src/haproxy-1.7.5.tar.gz'
-default['haproxy']['source_checksum'] = 'b04d7db6383c662eb0a421a95af7becac6d9744a1abf0df6b0280c1e61416121'
