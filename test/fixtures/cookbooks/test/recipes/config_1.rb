d765b6cf458f55914f581e5b33a80fb3a355b66f
Manuel Felici <manuel.felici@sourcesense.com>
2017-04-28 15:35:54 +0200

    Added test for multiple bind

--- test/fixtures/cookbooks/test/recipes/config_1.rb
+++ test/fixtures/cookbooks/test/recipes/config_1.rb
@@ -25,6 +25,11 @@ haproxy_frontend 'http-in' do
   default_backend 'servers'
 end
 
+haproxy_frontend 'multiport' do
+  bind ({'*' => '8080', '0.0.0.0' => ['8081', '8180']})
+  default_backend 'servers'
+end
+
 haproxy_backend 'servers' do
   server ['server1 127.0.0.1:8000 maxconn 32']
 end
