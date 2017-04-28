f3923e8bc1bb7f4f36f7feb89d247307d9657497
Richard Shade <rshade@rightscale.com>
2017-04-28 08:44:03 -0500

    adding userlist test

---
+++ test/integration/config_1_userlist/example_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+title 'Frontend & Backend should be configurable'
+
+describe package('haproxy') do
+  it { should be_installed }
+end
+
+describe directory '/etc/haproxy' do
+  it { should exist }
+end
+
+describe file('/etc/haproxy/haproxy.cfg') do
+  it { should exist }
+  it { should be_owned_by 'haproxy' }
+  it { should be_grouped_into 'haproxy' }
+  its('content') { should match(/daemon/) }
+  its('content') { should match(/timeout connect 5000ms/) }
+  its('content') { should match(/frontend http-in/) }
+  its('content') { should match(/bind \*:80/) }
+  its('content') { should match(/backend servers/) }
+  its('content') { should match(/server server1 127.0.0.1:8000 maxconn 32/) }
+end
+
+describe service('haproxy') do
+  it { should be_running }
+end
