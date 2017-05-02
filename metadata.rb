6d52e79514b72049714b1464c1c84a4faa0fdb39
Will Fisher <will@alaska.edu>
2017-05-02 10:21:51 -0800

    fix license name for foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'haproxy'
 maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Installs and configures haproxy'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '4.1.0'
