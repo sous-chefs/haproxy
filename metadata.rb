fd991ebbc3f913fca620efd257709e0776c26a96
Richard Shade <rshade@rightscale.com>
2017-05-04 09:09:27 -0500

    locking deps

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures haproxy'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '4.1.0'
+version           '4.2.0'
 source_url        'https://github.com/sous-chefs/haproxy'
 issues_url        'https://github.com/sous-chefs/haproxy/issues'
 chef_version      '>= 12.5' if respond_to?(:chef_version)
@@ -14,6 +14,6 @@ chef_version      '>= 12.5' if respond_to?(:chef_version)
 end
 
 depends           'cpu', '>= 0.2.0'
-depends           'build-essential'
-depends           'poise-service'
+depends           'build-essential', '>= 8.0.1'
+depends           'poise-service',   '>= 1.5.1'
 depends           'compat_resource', '>= 12.16'
