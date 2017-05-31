b17b481c13357878ce6b2914dca4f7194175066a
Richard Shade <rshade@rightscale.com>
2017-05-31 13:20:44 -0500

    removing required

--- resources/frontend.rb
+++ resources/frontend.rb
@@ -2,7 +2,7 @@ property :name, String, name_property: true
 property :bind, [String, Hash], default: '0.0.0.0:80'
 property :mode, String, equal_to: %w(http tcp)
 property :maxconn, Integer, default: 2000
-property :default_backend, String, required: true
+property :default_backend, String
 property :use_backend, Array
 property :acl, Array
 property :option, Array
