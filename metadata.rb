e23466bc6654e4b64ce61ec820e454a18b631e93
Richard Shade <rshade@rightscale.com>
2017-06-13 10:27:06 -0500

    updating release, closes #230

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures haproxy'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '4.3.0'
+version           '4.3.1'
 source_url        'https://github.com/sous-chefs/haproxy'
 issues_url        'https://github.com/sous-chefs/haproxy/issues'
 chef_version      '>= 12.5' if respond_to?(:chef_version)
