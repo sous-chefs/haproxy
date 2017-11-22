7784e6571681d6f25fae8eed71efeb9c92d40229
Akmal Abbasov <akmal.abbasov@hivestreaming.com>
2017-11-22 17:44:11 +0100

    Add missing checks to test listen resource

--- test/fixtures/cookbooks/test/recipes/config_4.rb
+++ test/fixtures/cookbooks/test/recipes/config_4.rb
@@ -13,4 +13,12 @@ haproxy_listen 'admin' do
   stats uri: '/',
         realm: 'Haproxy-Statistics',
         auth: 'user:pwd'
+  http_request 'add-header X-Proto http'
+  http_response 'set-header Expires %[date(3600),http_date]'
+  default_backend 'servers'
+  extra_options('bind-process' => 'odd')
+end
+
+haproxy_backend 'servers' do
+  server ['disabled-server 127.0.0.1:1 disabled']
 end
