c805b4e0f4680a7167b86cf691e1a9f29368caca
Richard Shade <rshade@rightscale.com>
2018-02-16 15:33:45 -0600

    trying equals (#280)
    * trying equals
    
    * change ordering
    
    * excluding test on ubuntu
    
    * adding if
    
    * fixing start_with
    
    * fix source test
    
    * fixing cookstyle

--- test/integration/package/package_spec.rb
+++ test/integration/package/package_spec.rb
@@ -11,8 +11,15 @@ describe file '/etc/haproxy/haproxy.cfg' do
   its(:mode) { should cmp '0644' }
 end
 
-describe service 'haproxy' do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
+if os.debian? && os.release.start_with?('14')
+  describe service 'haproxy' do
+    it { should be_installed }
+    it { should be_running }
+  end
+else
+  describe service 'haproxy' do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
 end
