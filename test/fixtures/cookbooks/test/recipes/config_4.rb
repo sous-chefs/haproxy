5bfea31a0fbc306fb2496fe51b76eb36bebf6587
Stephen Hoekstra <shoekstra@schubergphilis.com>
2019-01-09 15:50:25 +0100

    Add hash_type param

--- test/fixtures/cookbooks/test/recipes/config_4.rb
+++ test/fixtures/cookbooks/test/recipes/config_4.rb
@@ -3,7 +3,9 @@ haproxy_install 'package'
 
 haproxy_config_global ''
 
-haproxy_config_defaults ''
+haproxy_config_defaults '' do
+  hash_type 'consistent'
+end
 
 haproxy_listen 'admin' do
   bind '0.0.0.0:1337'
@@ -18,10 +20,12 @@ haproxy_listen 'admin' do
   http_response 'set-header Expires %[date(3600),http_date]'
   default_backend 'servers'
   extra_options('bind-process' => 'odd')
+  hash_type 'consistent'
 end
 
 haproxy_backend 'servers' do
   server ['disabled-server 127.0.0.1:1 disabled']
+  hash_type 'consistent'
 end
 
 haproxy_service 'haproxy'
