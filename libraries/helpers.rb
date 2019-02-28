5d9660fde9e71fe4f7205926cc2039b849d55030
Dan Webb <dan.webb@damacus.io>
2019-02-28 17:59:37 +0000

    Move system command to a helper

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -5,6 +5,14 @@ class Chef
         v = Mixlib::ShellOut.new("haproxy -v | grep version | awk '{ print $3 }'")
         v.run_command.stdout.to_f
       end
+
+      def systemd_command(bin_prefix)
+        if haproxy_version < 1.8
+          ::File.join(bin_prefix, 'sbin', 'haproxy-systemd-wrapper')
+        else
+          ::File.join(bin_prefix, 'sbin', 'haproxy') + ' -Ws'
+        end
+      end
     end
   end
 end
