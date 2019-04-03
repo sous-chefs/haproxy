4e328c812dd173c429073a4f146d80f0becaccb4
John Roesler <johnrroesler@gmail.com>
2019-04-03 15:19:55 -0500

    update integration tests to follow content convention

--- test/integration/config_array/example_spec.rb
+++ test/integration/config_array/example_spec.rb
@@ -9,28 +9,67 @@ describe directory '/etc/haproxy' do
   it { should exist }
 end
 
+cfg_content = [
+  'global',
+  '  user haproxy',
+  '  group haproxy',
+  '  log /dev/log local0',
+  '  log-tag WARDEN',
+  '  chroot /var/lib/haproxy',
+  '  daemon',
+  '  quiet',
+  '  stats socket /var/lib/haproxy/stats level admin',
+  '  maxconn 256',
+  '  pidfile /var/run/haproxy\.pid',
+  '  tune\.bufsize 262144',
+  '',
+  '',
+  'defaults',
+  '  timeout connect 5000ms',
+  '  timeout client 5000ms',
+  '  timeout server 5000ms',
+  '  log global',
+  '  mode http',
+  '  balance roundrobin',
+  '  option httplog',
+  '  option dontlognull',
+  '  option redispatch',
+  '  option tcplog',
+  '  retries 5',
+  '  stats uri /haproxy-status',
+  '',
+  '',
+  'frontend http-in',
+  '  default_backend servers',
+  '  bind \*:80',
+  '  redirect prefix http://www\.bar\.com code 301 if \{ hdr\(host\) -i foo\.com \}',
+  '  redirect prefix http://www\.bar\.com code 301 if \{ hdr\(host\) -i www\.foo\.com \}',
+  '',
+  'frontend tcp-in',
+  '  mode tcp',
+  '  default_backend tcp-servers',
+  '  bind \*:3307',
+  '',
+  'frontend multiport',
+  '  default_backend servers',
+  '  bind \*:8080',
+  '  bind 0\.0\.0\.0:8081',
+  '  bind 0\.0\.0\.0:8180',
+  '',
+  '',
+  'backend servers',
+  '  server server1 127\.0\.0\.1:8000 maxconn 32',
+  '',
+  'backend tcp-servers',
+  '  mode tcp',
+  '  server server2 127\.0\.0\.1:3306 maxconn 32',
+]
+
 describe file('/etc/haproxy/haproxy.cfg') do
   it { should exist }
   it { should be_owned_by 'haproxy' }
   it { should be_grouped_into 'haproxy' }
-  its('content') { should match(/^  daemon$/) }
-  its('content') { should match(/^  timeout connect 5000ms$/) }
-  its('content') { should match(/^  maxconn 256$/) }
-  its('content') { should match(%r{^  stats socket /var/lib/haproxy/stats level admin$}) }
-  its('content') { should match(%r{^  stats uri /haproxy-status$}) }
-  its('content') { should match(/^frontend http-in$/) }
-  its('content') { should match(%r{^  redirect prefix http://www\.bar\.com code 301 if \{ hdr\(host\) -i foo\.com \}$}) }
-  its('content') { should match(%r{^  redirect prefix http://www\.bar\.com code 301 if \{ hdr\(host\) -i www\.foo\.com \}$}) }
-  its('content') { should match(/^frontend multiport$/) }
-  its('content') { should match(/^  bind \*:80$/) }
-  its('content') { should match(/^  bind \*:8080$/) }
-  its('content') { should match(/^  bind 0.0.0.0:8081$/) }
-  its('content') { should match(/^  bind 0.0.0.0:8180$/) }
-  its('content') { should match(/^backend servers$/) }
-  its('content') { should match(/^  default_backend servers$/) }
-  its('content') { should match(/^  server server1 127.0.0.1:8000 maxconn 32$/) }
-  its('content') { should match(/^frontend tcp-in\n  mode tcp$/) }
-  its('content') { should match(/^backend tcp-servers\n  mode tcp$/) }
+  its('content') { should match(/#{cfg_content.join('\n')}/) }
 end
 
 describe service('haproxy') do
