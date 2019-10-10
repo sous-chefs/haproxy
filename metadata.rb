4f0015c7aa8a4dd51b67ee148f94ef2c23ec85c8
Marcos Oliveira <marcosvm@gmail.com>
2019-10-09 22:22:33 -0700

    Run latest cookstyle

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures haproxy'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version           '8.1.1'
 source_url        'https://github.com/sous-chefs/haproxy'
 issues_url        'https://github.com/sous-chefs/haproxy/issues'
