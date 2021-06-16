f61a82012d1b1861089590fd13854171750d93bb
Dan Webb <dan.webb@damacus.io>
2021-06-16 12:17:31 +0100

    Remove the Berksfile
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- Policyfile.rb
+++ Policyfile.rb
@@ -3,11 +3,9 @@ name 'haproxy'
 default_source :supermarket, 'https://supermarket.chef.io'
 cookbook 'haproxy', path: '.'
 cookbook 'test', path: 'test/cookbooks/test'
-
 run_list 'haproxy'
 
-tests = (Dir.entries('./test/cookbooks/test/recipes').select { |f| !File.directory? f })
-tests.each do |test|
+Dir.each_child('./test/cookbooks/test/recipes'). do |test|
   test = test.gsub('.rb', '')
-  named_run_list :"#{test.to_sym}", "test::#{test}"
+  named_run_list test.to_sym, "test::#{test}"
 end
