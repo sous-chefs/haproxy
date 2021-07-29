1cdc577e1ada997edc5dbc24fe88250aae22b88e
Dan Webb <dan.webb@damacus.io>
2021-07-29 10:54:54 +0100

    More Policyfile improvements!

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,11 +1,11 @@
 name 'haproxy'
 
 default_source :supermarket, 'https://supermarket.chef.io'
-cookbook 'haproxy', path: '.'
+metadata
 cookbook 'test', path: 'test/cookbooks/test'
+
 run_list 'haproxy'
 
-Dir.each_child('./test/cookbooks/test/recipes'). do |test|
-  test = test.gsub('.rb', '')
-  named_run_list test.to_sym, "test::#{test}"
+Dir.each_child('./test/cookbooks/test/recipes') do |test|
+  named_run_list :"#{File.basename(test, '.*')}".to_sym, "test::#{File.basename(test, '.*')}"
 end
